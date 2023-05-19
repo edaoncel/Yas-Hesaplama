51f0488318e59070b8f37efb8df0f39fa2105ff3
eda oncel <edaoncel@email.com>
2023-05-19 14:50:34 +0300

    Yaş Hesaplama Projesi

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace yas_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDogumtarihi = new System.Windows.Forms.TextBox();
+            this.lblDogumtarihi = new System.Windows.Forms.Label();
+            this.btnYashesaplayıcı = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDogumtarihi
+            // 
+            this.txtDogumtarihi.Location = new System.Drawing.Point(324, 90);
+            this.txtDogumtarihi.Name = "txtDogumtarihi";
+            this.txtDogumtarihi.Size = new System.Drawing.Size(137, 26);
+            this.txtDogumtarihi.TabIndex = 0;
+            // 
+            // lblDogumtarihi
+            // 
+            this.lblDogumtarihi.AutoSize = true;
+            this.lblDogumtarihi.Location = new System.Drawing.Point(139, 96);
+            this.lblDogumtarihi.Name = "lblDogumtarihi";
+            this.lblDogumtarihi.Size = new System.Drawing.Size(96, 20);
+            this.lblDogumtarihi.TabIndex = 1;
+            this.lblDogumtarihi.Text = "dogum tarihi";
+            this.lblDogumtarihi.Click += new System.EventHandler(this.lblDogumtarihi_Click);
+            // 
+            // btnYashesaplayıcı
+            // 
+            this.btnYashesaplayıcı.Location = new System.Drawing.Point(324, 172);
+            this.btnYashesaplayıcı.Name = "btnYashesaplayıcı";
+            this.btnYashesaplayıcı.Size = new System.Drawing.Size(137, 38);
+            this.btnYashesaplayıcı.TabIndex = 2;
+            this.btnYashesaplayıcı.Text = "yas hesaplayıcı";
+            this.btnYashesaplayıcı.UseVisualStyleBackColor = true;
+            this.btnYashesaplayıcı.Click += new System.EventHandler(this.btnYashesaplayıcı_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnYashesaplayıcı);
+            this.Controls.Add(this.lblDogumtarihi);
+            this.Controls.Add(this.txtDogumtarihi);
+            this.Name = "Form1";
+            this.Text = "yas hesaplayıcı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDogumtarihi;
+        private System.Windows.Forms.Label lblDogumtarihi;
+        private System.Windows.Forms.Button btnYashesaplayıcı;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yas_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDogumtarihi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYashesaplayıcı_Click(object sender, EventArgs e)
+        {
+            DateTime Dogumtarih;
+            Dogumtarih = Convert.ToDateTime(txtDogumtarihi.Text);
+            int yas;
+            yas = DateTime.Now.Year - Dogumtarih.Year;
+            MessageBox.Show(yas.ToString());
+        }
+    }
+}
